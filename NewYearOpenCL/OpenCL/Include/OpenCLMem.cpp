89f802a54e6ffe85cf5ae55faec7371d2606291c
Haomin Kong <a645162@gmail.com>
2023-12-15 14:26:54 +0800

    update File Struct

---
+++ NewYearOpenCL/OpenCL/Include/OpenCLMem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 孔昊旻 on 2023/12/15.
+//
+
+#include "OpenCLMem.h"
---
+++ NewYearOpenCL/OpenCL/Include/OpenCLMem.h
@@ -0,0 +1,8 @@
+//
+// Created by 孔昊旻 on 2023/12/15.
+//
+
+#ifndef NEWYEAROPENCL_OPENCLMEM_H
+#define NEWYEAROPENCL_OPENCLMEM_H
+
+#endif //NEWYEAROPENCL_OPENCLMEM_H
