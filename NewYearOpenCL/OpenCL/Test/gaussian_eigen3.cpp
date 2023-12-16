fc170256f3394bc1b897b49b1f3d21d7ef89fb24
Haomin Kong <a645162@qq.com>
2023-12-16 14:13:35 +0800

    try to use avx2

--- NewYearOpenCL/OpenCL/Test/gaussian_eigen3.cpp
+++ NewYearOpenCL/OpenCL/Test/gaussian_eigen3.cpp
@@ -6,6 +6,13 @@
 #include <eigen3/Eigen/Dense>
 #include <chrono>
 
+#include <cmath>
+#include <immintrin.h>
+
+#define M_PI 3.14159265358979323846
+
+#define SIZE 28
+
 Eigen::MatrixXf createGaussianKernel(int size, float strength) {
     Eigen::MatrixXf kernel(size, size);
 
@@ -20,32 +27,48 @@ Eigen::MatrixXf createGaussianKernel(int size, float strength) {
         }
     }
 
-//    float sum=0;
-//    for (int x = 0; x < size; ++x) {
-//        for (int y = 0; y < size; ++y) {
-//            sum+=kernel(x, y);
-//        }
-//    }
-//
-//    for (int x = 0; x < size; ++x) {
-//        for (int y = 0; y < size; ++y) {
-//            kernel(x, y) /= sum;
-//        }
-//    }
-//    return kernel;
     return kernel / kernel.sum();
 }
 
 int main() {
-    int size = 28; // Adjust the size as needed
+
+    int size = SIZE; // Adjust the size as needed
     float strength = 9.1f; // Adjust the strength as needed
 
     auto time1 = std::chrono::high_resolution_clock::now();
     Eigen::MatrixXf gaussianKernel = createGaussianKernel(size, strength);
     auto time2 = std::chrono::high_resolution_clock::now();
     auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(time2 - time1).count();
-    std::cout << "Time taken to create the Gaussian kernel: " << duration1 << " microseconds" << std::endl;
+    std::cout << "(Eigen)Time taken to create the Gaussian kernel: " << duration1 << " microseconds" << std::endl;
 
+    auto time3 = std::chrono::high_resolution_clock::now();
+    float *kernel;
+    kernel = (float *) malloc(size * size * sizeof(float));
+    float sum = 0;
+//#pragma omp parallel for num_threads(1)
+    for (int x = 0; x < size; ++x) {
+        for (int y = 0; y < size; ++y) {
+            float value = (1 / (2 * M_PI * std::pow(strength, 2))) *
+                          std::exp(-((x - (size - 1) / 2.0f) * (x - (size - 1) / 2.0f) +
+                                     (y - (size - 1) / 2.0f) * (y - (size - 1) / 2.0f)) /
+                                   (2 * std::pow(strength, 2)));
+            kernel[x * size + y] = value;
+            sum += value;
+        }
+    }
+//#pragma omp parallel for num_threads(1)
+    for (int x = 0; x < size; ++x) {
+        for (int y = 0; y < size; ++y) {
+            kernel[x * size + y] /= sum;
+        }
+    }
+
+    auto time4 = std::chrono::high_resolution_clock::now();
+    auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(time4 - time3).count();
+    std::cout << "(OpenMP)Time taken to create the Gaussian kernel: " << duration2 << " microseconds" << std::endl;
+
+
+    std::cout << "Sum: " << sum << std::endl;
 
     // Print the resulting Gaussian kernel
 //    std::cout << "Gaussian Kernel:" << std::endl << gaussianKernel << std::endl;
@@ -59,12 +82,26 @@ int main() {
 //    }
 
     float *data = gaussianKernel.data();
+
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            printf("%.6f  ", data[i * size + j]);
+            data[i * size + j] -= kernel[i * size + j];
         }
-        printf("\n");
     }
 
+    std::cout << "diff: " << std::endl;
+    float diff = gaussianKernel.sum();
+    double roundedNumber = round(diff * 100000) / 100000;
+
+    std::cout << roundedNumber << std::endl;
+
+//    for (int i = 0; i < size; i++) {
+//        for (int j = 0; j < size; j++) {
+//            printf("%.6f  ", data[i * size + j]);
+//        }
+//        printf("\n");
+//    }
+
+    free(kernel);
     return 0;
 }
